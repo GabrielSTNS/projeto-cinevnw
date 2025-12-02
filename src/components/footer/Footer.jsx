2590c05887f4c6b04c8e40592a9f8d86d51a0fa9
GabrielSTNS <gabriel2000.santana@gmail.com>
2025-12-02 01:44:15 -0300

    ✨ feat: Atualização do Footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import S from './Footer.module.scss'
 const Footer = () => {
   return (
     <footer className={S.footer}>
-      <p>&copy; Gabriel Santana em parceria com a escola Vai na Web. Todos os direitos reservados.</p>
+      <p>&copy; 2025 Gabriel Santana em parceria com a escola Vai na Web. Todos os direitos reservados.</p>
       <div>
         <a href="https://www.linkedin.com/in/gabrielsnt/" target='_blank'><img src="https://img.icons8.com/arcade/64/linkedin-circled.png" alt="linkedin"/></a>
         <a href="https://github.com/GabrielSTNS" target='_blank'><img src="https://img.icons8.com/arcade/64/github.png" alt="github"/></a>
